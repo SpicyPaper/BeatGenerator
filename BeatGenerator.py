10365bcb9874eed834edbaca991c7db82d668a58
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-19 17:07:26 +0100

    Improve main, complete and improve diffBetween2Images methods

--- BeatGenerator.py
+++ BeatGenerator.py
@@ -6,9 +6,49 @@ import argparse
 import random
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-imgCounter = 1
+def diffBetween2Images(cap, everyNImages, th):
 
-def diffBetween2Images(previousFrame, frame, th):
+    degrees = []  # MIDI note number
+    counter = 0
+    previousNbOfDiff = -1
+
+    if(cap.isOpened()):
+        ret, frame = cap.read()
+    
+    while(cap.isOpened()):
+        previousFrame = frame
+        ret, frame = cap.read()
+
+        if frame is None:
+            break
+        else:
+            if counter % everyNImages == 0:
+                previousNbOfDiff, sound = __diffBetween2Images(previousNbOfDiff, previousFrame, frame, th)
+                if sound != -1:
+                    degrees.append(sound)
+            counter += 1
+
+    return degrees
+
+def __diffBetween2Images(previousNbOfDiff, previousFrame, frame, th):
+    
+    diff = cv2.absdiff(previousFrame, frame)
+    mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+    imask = mask > th
+    nbOfDiff = np.sum(imask)
+
+    if previousNbOfDiff == -1:
+        return nbOfDiff, -1
+    else:
+        frameSize = np.sum(np.ones_like(frame, np.uint8))
+        diffBetweenDiff = np.absolute(nbOfDiff - previousNbOfDiff)
+        
+        sound = int(diffBetweenDiff / frameSize * 60)
+
+        return np.sum(imask), sound
+
+def __diffBetween2Images_display(previousFrame, frame, th, imgCounter):
     
     diff = cv2.absdiff(previousFrame, frame)
     mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -18,52 +58,71 @@ def diffBetween2Images(previousFrame, frame, th):
     canvas = np.zeros_like(frame, np.uint8)
     canvas[imask] = frame[imask]
 
-    global imgCounter
-    cv2.imwrite("Diff/result" + str(imgCounter) + ".png", canvas)
-    imgCounter += 1
+    cv2.imwrite("DiffImages/result" + str(imgCounter) + ".png", canvas)
 
-def main_diffBetween2Images(cap, th):
+def averageRGB(cap, everyNPixels, everyNImages):
+    """
+    Compute the average color of a given video
+    """
 
     degrees = []  # MIDI note number
-    counter = 0
+    imagesCounter = 0
 
     if(cap.isOpened()):
         ret, frame = cap.read()
     
     while(cap.isOpened()):
-        previousFrame = frame
         ret, frame = cap.read()
-        everyNImages = 1
+        imagesCounter += 1
 
         if frame is None:
             break
         else:
-            if counter % everyNImages == 0:
-                #degrees.append(averageRGB(frame, 100))
-                degrees.append(diffBetween2Images(previousFrame, frame, th))
-            counter += 1
+            if imagesCounter % everyNImages == 0:
+                averageR = __averageRGBChoiceOneFrame(frame, 0, everyNPixels)
+                averageG = __averageRGBChoiceOneFrame(frame, 1, everyNPixels)
+                averageB = __averageRGBChoiceOneFrame(frame, 2, everyNPixels)
 
-def averageRGB(frame, everyNPixels):
+                averageColor = int((averageR + averageG + averageB) / 3)
+
+                degrees.append(averageColor)
+
+    return degrees
+
+def averageRGBStage(cap, everyNPixels, everyNImages, colorStage):
     """
     Compute the average color of a given video
     """
-    averageR = averageRGBChoice(frame, 0, everyNPixels)
-    averageG = averageRGBChoice(frame, 1, everyNPixels)
-    averageB = averageRGBChoice(frame, 2, everyNPixels)
 
-    averageColor = int((averageR + averageG + averageB) / 3)
+    degrees = []  # MIDI note number
+    imagesCounter = 0
 
-    print(averageColor)
+    if(cap.isOpened()):
+        ret, frame = cap.read()
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        imagesCounter += 1
 
-    return averageColor
+        if frame is None:
+            break
+        else:
+            if imagesCounter % everyNImages == 0:
+                averageStage = __averageRGBChoiceOneFrame(frame, colorStage, everyNPixels)
+                degrees.append(averageStage)
 
-def averageRGBChoice(frame, colorStage, everyNPixels):
+    return degrees
+
+def __averageRGBChoiceOneFrame(frame, colorStage, everyNPixels):
     """
-    Compute the average of red, green or blue.
+    Compute a sound between [0 ; 255 / 4] based on the average of
+    red, green or blue in a given frame.
+
     frame : the images
     colorStage : 0 = red, 1 = green, 2 = blue
     everyNPixels : take one pixel every n pixels
     """
+
     vect = frame.shape
     dimX = vect[0]
     dimY = vect[1]
@@ -76,7 +135,7 @@ def averageRGBChoice(frame, colorStage, everyNPixels):
             if (x * y) % everyNPixels == 0:
                 nbPixels += 1
                 averageColor += frame[x][y][colorStage]
-    
+
     # [0 ; 255 / 4]
     averageColor = int(averageColor / nbPixels / 4)
 
@@ -102,38 +161,40 @@ if __name__ == "__main__":
 
     # args = vars(parser.parse_args())
 
-    videoNameTMP = 'Videos/Fond_Noir_Tissu_Cam_Fixe'
+    """Init"""
+    degrees = []
+    videoName = 'Fond_Noir_Tissu_Cam_Fixe_Start_Part'
+    cap = cv2.VideoCapture('Videos/' + videoName + '.avi')
 
-    cap = cv2.VideoCapture(videoNameTMP + '.avi')
+    """Apply algo on video"""
+    degrees = diffBetween2Images(cap, 10, 120)
+    print(degrees)
+    #degrees = averageRGB(cap, 100, 10)
+    #degrees = averageRGBStage(cap, 100, 10, 0)
 
-    main_diffBetween2Images(cap, 5)
-
+    """Release"""
     cap.release()
-    cv2.destroyAllWindows()
 
-    # track    = 0
-    # channel  = 0
-    # time     = 0    # In beats
-    # duration = 1    # In beats
-    # tempo    = 50   # In BPM
-    # volume   = 100  # 0-127, as per the MIDI standard
+    """Create the music based on previous info"""
+    track    = 0
+    channel  = 0
+    time     = 0    # In beats
+    duration = 1    # In beats
+    tempo    = 50   # In BPM
+    volume   = 100  # 0-127, as per the MIDI standard
 
-    # # Get the duration of the video
-    # clip = VideoFileClip(videoNameTMP + '.avi')
-    # print( clip.duration )  
-    # # Define the number of note the music must have according to the tempo
-    # numberOfNote = getNumberNote(clip.duration, tempo)
+    # Get the duration of the video
+    clip = VideoFileClip('Videos/' + videoName + '.avi')
+    print( clip.duration )  
+    # Define the number of note the music must have according to the tempo
+    numberOfNote = getNumberNote(clip.duration, tempo)
 
-    # # Generate the correct number of note to have a music of the desired length
-    # for i in range(numberOfNote):
-    #     degrees.append(random.randint(10, 100))
+    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+                        # automatically)
+    MyMIDI.addTempo(track, time, tempo)
 
-    # MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
-    #                     # automatically)
-    # MyMIDI.addTempo(track, time, tempo)
+    for i, pitch in enumerate(degrees):
+        MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
 
-    # for i, pitch in enumerate(degrees):
-    #     MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
-
-    # with open(videoNameTMP + '.mid', "wb") as output_file:
-    #     MyMIDI.writeFile(output_file)
+    with open('Sounds/' + videoName + '.mid', "wb") as output_file:
+        MyMIDI.writeFile(output_file)
