77605084ae9f5faa3b71e290ac0bd88447b2049a
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-05-04 08:55:26 +0200

    Removal of unused files

--- sources/BeatGenerator.py
+++ sources/BeatGenerator.py
@@ -1,14 +1,14 @@
 
 from Generator import Generator
 from Track import Track
-from MDItoMP3 import convertMdiToMp3
-from addMp3ToAvi import addMp3ToAvi
+
 if __name__ == "__main__":
     
-    gen = Generator("RGBW")
+    gen = Generator("Fond_Noir_Tissu_Cam_Fixe_2")
 
     gen.setTrackParams(0.5, 12)
-    gen.convolution(10, [[-1, 0, 1],[-1, 0, 1],[-1, 0, 1]])
+    # gen.convolution(10, [[1, 0, -1],[1, 0, -1],[1, 0, -1]])
+    gen.averageRGB()
     gen.createMidi(True)
     gen.convertMdiToMp3()
     gen.addMp3ToAvi()
