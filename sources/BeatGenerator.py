aa4c5ff533ddbc2474f8375f28c76ee7812ffe9b
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-05-07 00:00:54 +0200

    Corrections for convolution

--- sources/BeatGenerator.py
+++ sources/BeatGenerator.py
@@ -4,9 +4,9 @@ from Track import Track
 
 if __name__ == "__main__":
     
-    gen = Generator("RGBW")
+    gen = Generator("Edges_2")
     gen.setTrackParams(0.5, 12)
-    gen.averageRGBChannel(2)
+    gen.convolution(1, everyNPixels = 10, kernel = [[1, 0, -1],[1, 0, -1],[1, 0, -1]])
     gen.createMidi(True)
     gen.convertMdiToMp3()
     gen.addMp3ToAvi()
